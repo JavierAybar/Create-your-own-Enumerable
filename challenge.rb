37be9344447ea1763a3fdbfdf9a4448548148129
Ismael Mastronardi <mastronardiismael@gmail.com>
2023-09-27 14:14:33 -0300

    add module

--- challenge.rb
+++ challenge.rb
@@ -1,3 +1,27 @@
+module MyEnumerable
+  def pintar
+    each { |n| print n }
+  end
+
+  def all?(&block)
+    result = true
+    each { |n| result = false unless block.call(n) }
+    puts result
+  end
+
+  def any?(&block)
+    result = false
+    each { |n| result = true if block.call(n) }
+    puts result
+  end
+
+  def filter(&block)
+    result = []
+    each { |n| result << n if block.call(n) }
+    puts result.inspect
+  end
+end
+
 class MyList
   include MyEnumerable
 
